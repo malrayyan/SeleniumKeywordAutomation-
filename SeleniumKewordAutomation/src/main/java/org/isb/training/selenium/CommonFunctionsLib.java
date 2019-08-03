e9a03fd861c6d587d4cebb05b4bcff4e04a0249a
Al <malrayyan01@gmail.com>
2019-08-03 11:31:50 -0400

    Codes from Kabir

---
+++ SeleniumKewordAutomation/src/main/java/org/isb/training/selenium/CommonFunctionsLib.java
@@ -0,0 +1,221 @@
+package org.isb.training.selenium;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class CommonFunctionsLib {
+
+	final static Logger logger = Logger.getLogger(CommonFunctionsLib.class);
+
+	WebDriver driver;
+
+	CommonFunctionsLib(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public boolean performActions(String Keyword, String value, String xpathExpression)
+			throws NumberFormatException, InterruptedException {
+		//		logger.debug("Keyword: " +  Keyword + " value: " + value + " xpathExpression:  " + xpathExpression);
+		System.out.println("Keyword: " + Keyword + " value: " + value + " xpathExpression:  " + xpathExpression);
+		if (Keyword.equalsIgnoreCase("Click")) {
+			driver.findElement(By.xpath(xpathExpression)).click();
+			logger.info("Click is performed sucessfully");
+			return true;
+		} else if (Keyword.equalsIgnoreCase("Input")) {
+			driver.findElement(By.xpath(xpathExpression)).sendKeys(value);
+			logger.info("Input is performed sucessfully");
+			return true;
+		} else if (Keyword.equalsIgnoreCase("dropdown")) {
+			Select dropDown = new Select(driver.findElement(By.xpath(xpathExpression)));
+			dropDown.selectByVisibleText(value);
+
+		} else if (Keyword.equalsIgnoreCase("Radio-Male")) {
+
+			//Identifying Male radio button using its ID as an locator
+
+			WebElement maleRadioBtn = driver.findElement(By.id(xpathExpression));
+
+			//Checking if the Male Radio button is displayed on the Webpage and printing the status
+
+			boolean radioBtnIsDisplayed = maleRadioBtn.isDisplayed();
+
+			System.out.println("Is Male radio button displayed: "+radioBtnIsDisplayed);
+
+			//Checking if the Male Radio button is enabled on the webpage and printing the status
+
+			boolean radioBtnIsEnabled = maleRadioBtn.isEnabled();
+
+			System.out.println("Is Male radio button enabled: "+radioBtnIsEnabled);
+
+			//Checking the default radio button selection status
+
+			boolean radioBtnIsSelected = maleRadioBtn.isSelected();
+
+			System.out.println("Default Radio button selection Status: "+radioBtnIsSelected);
+
+			//Selecting male radio button
+
+			maleRadioBtn.click();
+
+			//rechecking the male radio button selection status and printing it..
+
+			boolean radioBtnNewSelectionStatus = maleRadioBtn.isSelected();
+
+			System.out.println("Male radio Selection status after perform click() event: "+radioBtnNewSelectionStatus);
+			//				driver.findElement(By.xpath(xpathExpression)).sendKeys(value);
+			//				logger.info("Input is performed sucessfully");
+			return true;
+		} else if (Keyword.equalsIgnoreCase("Radio-Female")) {
+
+			//			
+
+			//Identifying Male radio button using its ID as an locator
+
+			WebElement maleRadioBtn = driver.findElement(By.id(xpathExpression));
+
+
+
+			//Checking if the Male Radio button is displayed on the Webpage and printing the status
+
+			boolean radioBtnIsDisplayed = maleRadioBtn.isDisplayed();
+
+			System.out.println("Is Male radio button displayed: "+radioBtnIsDisplayed);
+
+
+
+			//Checking if the Male Radio button is enabled on the webpage and printing the status
+
+			boolean radioBtnIsEnabled = maleRadioBtn.isEnabled();
+
+			System.out.println("Is Male radio button enabled: "+radioBtnIsEnabled);
+
+
+
+			//Checking the default radio button selection status
+
+			boolean radioBtnIsSelected = maleRadioBtn.isSelected();
+
+			System.out.println("Default Radio button selection Status: "+radioBtnIsSelected);
+
+
+
+			//Selecting male radio button
+
+			maleRadioBtn.click();
+
+
+
+			//rechecking the male radio button selection status and printing it..
+
+			boolean radioBtnNewSelectionStatus = maleRadioBtn.isSelected();
+
+			System.out.println("Male radio Selection status after perform click() event: "+radioBtnNewSelectionStatus);
+			//				driver.findElement(By.xpath(xpathExpression)).sendKeys(value);
+			//				logger.info("Input is performed sucessfully");
+			return true;
+
+		} else if (Keyword.equalsIgnoreCase("Radio-exp")) {
+
+			//			
+
+			//Identifying Male radio button using its ID as an locator
+
+			WebElement expRadioBtn = driver.findElement(By.id(xpathExpression));
+
+
+
+			//Checking if the Male Radio button is displayed on the Webpage and printing the status
+
+			boolean radioBtnIsDisplayed = expRadioBtn.isDisplayed();
+
+			System.out.println("Is Male radio button displayed: "+radioBtnIsDisplayed);
+
+
+
+			//Checking if the Male Radio button is enabled on the webpage and printing the status
+
+			boolean radioBtnIsEnabled = expRadioBtn.isEnabled();
+
+			System.out.println("Is Male radio button enabled: "+radioBtnIsEnabled);
+
+
+
+			//Checking the default radio button selection status
+
+			boolean radioBtnIsSelected = expRadioBtn.isSelected();
+
+			System.out.println("Default Radio button selection Status: "+radioBtnIsSelected);
+
+
+
+			//Selecting male radio button
+
+			expRadioBtn.click();
+
+
+
+			//rechecking the male radio button selection status and printing it..
+
+			boolean radioBtnNewSelectionStatus = expRadioBtn.isSelected();
+
+			System.out.println("Experience  radio Selection status after perform click() event: "+radioBtnNewSelectionStatus);
+			//				driver.findElement(By.xpath(xpathExpression)).sendKeys(value);
+			//				logger.info("Input is performed sucessfully");
+			return true;
+
+			//			add elements here for add terms and condition button
+			//		} else if (Keyword.equalsIgnoreCase("Input")) {
+			//			driver.findElement(By.xpath(xpathExpression)).sendKeys(value);
+			//			logger.info("Input is performed sucessfully");
+			//			return true;
+		} else if (Keyword.equalsIgnoreCase("gettext")) {
+			driver.findElement(By.xpath(xpathExpression)).getText();
+			logger.info("getText is performed sucessfully");
+			return true;
+		} else if (Keyword.equalsIgnoreCase("url")) {
+			driver.get(value);
+			logger.info("Url is opened sucessfully");
+			return true;
+		} else if (Keyword.equalsIgnoreCase("CloseBrowser")) {
+			driver.close();
+			logger.info("Browser is closed sucessfully");
+			return true;
+		} else if (Keyword.equalsIgnoreCase("sleep")) {
+			if (value.equalsIgnoreCase("level1")) {
+				Thread.sleep(3000);
+			} else if (value.equalsIgnoreCase("level2")) {
+				Thread.sleep(7000);
+			} else if (value.equalsIgnoreCase("level3")) {
+				Thread.sleep(15000);
+			} else if (value.equalsIgnoreCase("level4")) {
+				Thread.sleep(20000);
+			} else if (value.equalsIgnoreCase("level4")) {
+				Thread.sleep(25000);
+			} else {
+				Thread.sleep(5000);
+			}
+			logger.info("sleep is done sucessfully");
+			return true;
+		} else if (Keyword.equalsIgnoreCase("VerifyText")) {
+
+			if (value.equalsIgnoreCase(driver.findElement(By.xpath(xpathExpression)).getText())) {
+				logger.info("verify text true");
+				return true;
+			} else {
+				logger.info("verify text failed");
+				return false;
+			}
+
+		} else if (Keyword.equalsIgnoreCase("Pop")) {
+			// Do nothing
+		}
+
+		return false;
+	}
+
+}
